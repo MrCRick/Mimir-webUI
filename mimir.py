f109b2bdc64b2314bb4b2d1dab9706e4eb461165
MrCrick <riccardo.cicero01@gmail.com>
2020-12-08 16:18:24 +0100

    mod

---
+++ mimir.py
@@ -0,0 +1,15 @@
+import click
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option('--count', default=1, help='number of greetings')
+@click.argument('name')
+def hello(count, name):
+    for x in range(count):
+        click.echo('Hello %s!' % name)
+
+
+cli.add_command(hello)
